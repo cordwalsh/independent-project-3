4e9cba36285713de01193a01f16a0cfd66d70e00
cord walsh <cordwalsh@MacBook-Pro-3.local>
2019-06-14 11:26:37 -0700

    adds javascript, if,else if statements

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Spec:
 The program returns a range of numbers from 0 to the users inputted number
 - Input: "4"
 - Output: "0, 1, 2, 3, 4"
+
+The program returns a phrase depending on the users inputted numbers
+- Input: "1"
+- Output: Beep!
+
+The program returns a phrase depending on the users inputted numbers
+- Input: "2"
+- Output: "Boop!"
+
+The program returns a phrase depending on the users inputted numbers
+- Input: "3"
+- Output: "I'm sorry Dave. I'm afraid I can't do that."
+
+The program returns a phrase when the user inputs a phrase.
+- Input: "hello"
+- Output "Please enter a number that I can read"
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    var keyNumbers= ['1','2','3']
+    var userInput = ($("#number").val());
+    var finalString = "";
+    var stringArray = [];
+// ////////////////////////
+    console.log(userInput)
+    for (var index = 0; index <= userInput; index ++) {
+      var indexString = index.toString();
+      if (indexString.includes("3")) {
+        // finalString+= " I'm sorry, I'm afraid I can't do that"
+        stringArray[index] = " I'm sorry, I'm afraid I can't do that"
+      }
+      else if (indexString.includes("2")) {
+        // finalString += " Boop!"
+        stringArray[index] = " Boop!"
+      }
+      else if (indexString.includes("1")) {
+          // finalString += " Beep!"
+          stringArray[index] = " Beep!"
+      }
+      else {
+        // finalString += " " + indexString;
+        stringArray[index]= indexString;
+      }
+
+
+      console.log(indexString)
+    }
+  $("#results").append(stringArray.toString());
 
   });
 });
